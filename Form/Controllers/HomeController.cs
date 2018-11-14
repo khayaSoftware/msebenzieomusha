7285fbfc85c1edc9f005574bbe9440eae848d3f8
ValiBeauty <valibea100>
2018-11-14 00:26:11 +0000

    init

---
+++ Form/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form.Controllers
+{
+    public class HomeController : Controller
+    {
+        private String PreSharedKey = "5XNm04UE0/EaXDSeKa4Fw29hTzieAl32uO4=";
+        public ActionResult Index()
+        {
+            Payment PaymentForForm = new Payment();
+            PaymentForForm.HashDigest = Hash(PreSharedKey);
+
+            return View(PaymentForForm);
+            
+        }
+        public static string Hash(string stringToHash)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                return BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(stringToHash)));
+            }
+        }
+    }
+}
---
+++ Form/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form.Models
+{
+    public class Payment
+    {
+        public String HashDigest { get; set; }
+
+    }
+}

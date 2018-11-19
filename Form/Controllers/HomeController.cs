caa19ab06aafc4b3fdfa55fc591a50dc469590fa
ValiBeauty <valibea100>
2018-11-19 00:49:56 +0000

    Hashing needed

--- Form/Controllers/HomeController.cs
+++ Form/Controllers/HomeController.cs
@@ -12,20 +12,37 @@ namespace Form.Controllers
     public class HomeController : Controller
     {
         private String PreSharedKey = "5XNm04UE0/EaXDSeKa4Fw29hTzieAl32uO4=";
+
         public ActionResult Index()
         {
             Payment PaymentForForm = new Payment();
-            PaymentForForm.HashDigest = Hash(PreSharedKey);
+
+            // Initialise Data (Ideally would be read from some sort of DB :/)
+            PaymentForForm.HashDigest = Payment.Hash(PreSharedKey, "G6CH6Z90I2");
+            PaymentForForm.MerchantID = "Progra-7702818";
+            PaymentForForm.Amount = 20.00;
+            PaymentForForm.EchoAVSCheckResult = true;
+            PaymentForForm.EchoCV2CheckResult = true;
+            PaymentForForm.EchoThreeDSecureAuthenticationCheckResult = true;
+            PaymentForForm.EchoCardType = true;
+            PaymentForForm.AVSOverridePolicy = "PPPP";
+            PaymentForForm.CV2Overridepolicy = "PP";
+            PaymentForForm.ThreeDSecureOverridePolicy = "true";
+            PaymentForForm.OrderID = "Order-1234";
+            PaymentForForm.TransactionType = "SALE";
+            PaymentForForm.TransactionDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss K");
+            PaymentForForm.CallbackURL = "http://localhost:51772/Home/Index";
+            PaymentForForm.EmailAddressEditable = true;
+            PaymentForForm.PhoneNumberEditable = true;
+            PaymentForForm.CV2Mandatory = true;
+            PaymentForForm.PostCodeMandatory = true;
+            PaymentForForm.StateMandatory = true;
+            PaymentForForm.CountryMandatory = true;
+            PaymentForForm.ResultDeliveryMethod = "POST";
 
             return View(PaymentForForm);
             
         }
-        public static string Hash(string stringToHash)
-        {
-            using (var sha1 = new SHA1Managed())
-            {
-                return BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(stringToHash)));
-            }
-        }
+        
     }
 }
--- Form/Models/Payment.cs
+++ Form/Models/Payment.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace Form.Models
@@ -8,6 +10,35 @@ namespace Form.Models
     public class Payment
     {
         public String HashDigest { get; set; }
+        public String MerchantID { get; set; }
+        public double Amount { get; set; }
+        public String Currency { get; set; }
+        public bool EchoAVSCheckResult { get; set; }
+        public bool EchoCV2CheckResult { get; set; }
+        public bool EchoThreeDSecureAuthenticationCheckResult { get; set; }
+        public bool EchoCardType { get; set; }
+        public String AVSOverridePolicy { get; set; }
+        public String CV2Overridepolicy { get; set; }
+        public String ThreeDSecureOverridePolicy { get; set; }
+        public String OrderID { get; set; }
+        public String TransactionType { get; set; }
+        public String TransactionDateTime { get; set; }
+        public String CallbackURL { get; set; }
+        public bool EmailAddressEditable { get; set; }
+        public bool PhoneNumberEditable { get; set; }
+        public bool CV2Mandatory { get; set; }
+        public bool Address1Mandatory { get; set; }
+        public bool CityMandatory { get; set; }
+        public bool PostCodeMandatory { get; set; }
+        public bool StateMandatory { get; set; }
+        public bool CountryMandatory { get; set; }
+        public String ResultDeliveryMethod { get; set; }
 
+        public static string Hash(string salt, string message)
+        {
+            var provider = MD5.Create();
+            byte[] bytes = provider.ComputeHash(Encoding.ASCII.GetBytes(salt + message));
+            return BitConverter.ToString(bytes);
+        }
     }
 }
